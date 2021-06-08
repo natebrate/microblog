b3266584215785252b7dda1dd263c2c252a780d9
natebrate <nbrathwaite16@gmail.com>
2021-06-08 01:34:36 -0400

    added more bootstrap

---
+++ boot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# this script is used to boot a Docker container
+source venv/bin/activate
+flask db upgrade
+flask translate compile
+exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:ap
